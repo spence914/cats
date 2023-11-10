38a075f2b7014d921bbc77ec594e1c9ce82b51b7
Spencer Lewis <spence.914@gmail.com>
2023-11-10 14:35:25 -0800

    create functions and test files, learning async behaviour and syntax

---
+++ asyncBreeds.js
@@ -0,0 +1,16 @@
+// asyncBreeds.js
+const fs = require('fs');
+
+const breedDetailsFromFile = function(breed, done) {
+  console.log('breedDetailsFromFile: Calling readFile...');
+  fs.readFile(`./data/${breed}.txt`, 'utf8', (error, data) => {
+    console.log("In readFile's Callback: it has the data.");
+    if (!error) done(data);
+    if (error) done(undefined);
+  });
+};
+
+
+
+module.exports = breedDetailsFromFile;
+
